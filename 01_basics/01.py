39619cc235b1993969a376786d6c183ae603dc8e
bukharisaidh <bukharisaidh@gmail.com>
2023-02-13 18:07:52 +0300

    commit

---
+++ 01_basics/01.py
@@ -0,0 +1,9 @@
+print("hello python \n")
+
+user_name =input("Enter your name :")
+print(user_name)
+print(f"Your name is "+user_name)
+
+phone_number = input("Enter phone_number")
+print(phone_number)
+print(f"Your phone_number"+phone_number)
---
+++ 02_control_flew/01_def.py
@@ -0,0 +1,16 @@
+def simpleif():
+    first_input = int(input("Enter 1st number: "))
+    second_input = int(input("Enter 2nd number: "))
+
+    if first_input > second_input:
+        print("First input is greater")
+
+    if first_input < second_input:
+        print("Second input is greater")    
+
+
+def dd():
+    print("Inside second function")
+
+simpleif()
+print("hello")           
