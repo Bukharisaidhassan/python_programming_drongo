c95f3b44e9442c7a5e539202e9a6c8f83b84d27a
bukharisaidh <bukharisaidh@gmail.com>
2023-02-20 15:50:33 +0300

    added

---
+++ 02_control_flew/04.py
@@ -0,0 +1,10 @@
+def multiply(a: int, b: int):
+    print(a+b)
+multiply(5, 10)   
+
+
+def hello(name1,name2,name3):
+    print("Hello",name1,"\nHi", name2, "\nHey", name3)
+hello ("BK","CK","MK",)    
+
+
