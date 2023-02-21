bb83cb435aeb45015396b1e3dc48ee095e2ea3af
bukharisaidh <bukharisaidh@gmail.com>
2023-02-21 17:55:08 +0300

    added

---
+++ 01_basics/03.py
@@ -0,0 +1,10 @@
+# without paremeters, with the return statement
+def isEven():
+    x = 10
+    if (x % 2 == 0):
+        return "Even"
+    else:
+        return "Not Even"  
+even10 = isEven()
+print(even10)
+
