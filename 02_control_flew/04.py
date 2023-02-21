d67deadfd703b25f66a51ad679464b42b1d8a579
bukharisaidh <bukharisaidh@gmail.com>
2023-02-21 15:15:17 +0300

    commit

--- 02_control_flew/04.py
+++ 02_control_flew/04.py
@@ -1,10 +1,12 @@
 def multiply(a: int, b: int):
     print(a+b)
-multiply(5, 10)   
 
 
-def hello(name1,name2,name3):
-    print("Hello",name1,"\nHi", name2, "\nHey", name3)
-hello ("BK","CK","MK",)    
+multiply(5, 10)
 
 
+def hello(name1, name2, name3):
+    print("Hello", name1, "\nHi", name2, "\nHey", name3)
+
+
+hello("BK", "CK", "MK",)
