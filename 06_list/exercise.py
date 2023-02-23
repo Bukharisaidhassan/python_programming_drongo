c9ebdc4fcb042abe47af6acf642792512ce09dd6
bukharisaidh <bukharisaidh@gmail.com>
2023-02-23 15:05:06 +0300

    commit

---
+++ 06_list/exercise.py
@@ -0,0 +1,17 @@
+mylist1 = [10,20,30,40]
+mylist2 = [50,70,80,90]
+
+if mylist1 in mylist2:
+    print("strings are overlapping")
+
+else:
+    print("strings are NOT overlapping")
+
+if mylist1 is mylist:
+    print("Both values ")    
+
+list = 2005
+mylist = [100,110,2005,120,130,]
+if list is mylist:
+    print("list ")
+ 
