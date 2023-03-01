6eacb6c2361710612c375e7f930b334975cc15df
bukharisaidh <bukharisaidh@gmail.com>
2023-03-01 15:13:45 +0300

    commit

--- 07_condition.py/quiz2.py
+++ 07_condition.py/quiz2.py
@@ -1,15 +1,24 @@
 
-amout = int(input("Enter sale item : "))
 
-if (amout > 0):
-    if (amout <= 10000):
-        disc = amout * 0.02
-    elif amout <= 15000:
-        disc = amout * 0.03
-    elif amout <= 20000:
-        disc = amout * 0.04
+def get_discount():
+    item1 = int(input("Enter sale item : "))
 
-    else:
-        disc = 0.3 * amout
-    print("Discount : ", disc)
-    print("Net pay : ", amout)
+    item2 = int(input("Enter sale item : "))
+
+    amout = item1 + item2
+
+
+    if (amout > 0):
+        if (amout <= 10000):
+            disc = amout * 0.02
+        elif amout <= 15000:
+            disc = amout * 0.03
+        elif amout <= 20000:
+            disc = amout * 0.04
+
+        else:
+            disc = 0.05 * amout
+        print("Discount : ", disc)
+        print("Net pay : ", amout - disc)
+
+get_discount()
---
+++ 07_condition.py/range.py
@@ -0,0 +1,9 @@
+
+    
+Num = int(input("Enter the number multiplication: "))
+   
+for x in range(1,6):
+    result = Num * x
+
+    print(f"ans",result)    
+
